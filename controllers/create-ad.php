2c21054207ac67ef79cd278af17b4312ee8be76d
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-08-21 21:26:30 +0500

    Azizbek

--- controllers/create-ad.php
+++ controllers/create-ad.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 $branches = (new \App\Branch())->getBranches();
 
-loadView('dashboard/create_ad', ['branches' => $branches]);
+loadView('dashboard/create_ad', ['branches' => $branches]);git checkout <branch-name>
