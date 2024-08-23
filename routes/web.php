a814af799dd0d74c9d08a016b1bda232cc9af0e2
Azizbek201798 <ziyodulayeva9@gmail.com>
2024-08-23 14:55:20 +0500

    HW

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,25 @@
 declare(strict_types=1);
 
 use App\Router;
+use Controllers\AdController;
 
 Router::get('/', fn()=> loadController('home'));
 Router::get('/ads/{id}', function (int $id) {
     loadController('showAd', ['id'=>$id]);
 });
+
+Router::get('/login', fn()=> loadView('auth/login'));
+Router::get('/register', fn()=> loadView('auth/register'));
+
+Router::get('/ads/{id}', fn($id)=> (new AdController())->show($id));
+
+Router::get('/ads/create', fn()=> loadController('create-ad'));
+Router::post('/ads/create', fn()=>(new AdController())->create());
+
+Router::get('/logout', fn()=>(new \App\User())->logout());
+
+Router::post('/login', fn()=> loadController('auth/login'));
+Router::post('/register', fn()=> loadController('auth/register'));
 
 Router::get('/ads/create', fn()=> loadController('create-ad'));
 Router::post('/ads/create', fn()=> loadController('createAd'));
